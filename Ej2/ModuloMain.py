6d33fe1899ed0267c28da03eedaef17c0546db7e
Alejo Atienza <73915312+Alekitas@users.noreply.github.com>
2022-05-02 19:30:28 -0300

    Update ModuloMain.py

--- Ej2/ModuloMain.py
+++ Ej2/ModuloMain.py
@@ -1,38 +1,32 @@
-
-from ClaseViajeroFrecuente import ViajeroFrecuente
-import csv
-
-if __name__=='__main__':
-    lista=[]
-    c=0
-    archivo=open('Libro1.csv')
-    reader=csv.reader(archivo,delimiter=',')
-    for fila in reader:
-        numviajero=fila[0]
-        DNI=fila[1]
-        nombre=fila[2]
-        apellido=fila[3]
-        c+=1
-        millasacum=int(input('Ingrese la cantidad de millas acumuladas del viajero {}: \n'.format(numviajero)))
-    unViajero=ViajeroFrecuente(numviajero,DNI,nombre,apellido,millasacum)
-    lista.append(unViajero)
-    archivo.close()
-    opcion=0
-    while opcion!=-1:
-        print('1- Consultar Cantidad de Millas Acumuladas\n')
-        print('2-Acumular Millas\n')
-        print('3-Canjear Millas\n')
-        print('4-Ingrese -1 para finalizar\n')
-        if opcion==1:
-            for i in range(4):
-               print('Cantidad total de millas acumuladas del viajero: {} {}',lista[i].returnnumviajero(),';',lista[i].canttotalmillas())
-               if opcion==2:
-                   numv=input('Ingrese numero de viajero a acumular millas: ')
-                   act=input('Ingrese cantidad de millas del ultimo viaje: ')
-                   lista[numv-1].acumularmillas(act)
-                   print('Cantidad de millas actualizada: ',lista[i].canttotalmillas())
-                   if opcion==3:
-                       mcanj=input('Ingrese cantidad de millas a canjear: ')
-                       if mcanj<=lista[numv-1].canttotalmillas():
-                           lista[numv-1].canjearmillas(mcanj)
-                           print('Millas actuales: ',lista[i].canttotalmillas())
+class ViajeroFrecuente:
+    __numviajero=''
+    __DNI=''
+    __nombre=''
+    __apellido=''
+    __millasacum=''
+    
+    def __init__(self,numviajero,DNI,nombre,apellido,millasacum):
+        self.__numviajero=numviajero
+        self.__DNI=DNI
+        self.__nombre=nombre
+        self.__apellido=apellido
+        self.__millasacum=millasacum
+    
+    def muestra(self):
+        return self.__numviajero+';'+self.__DNI+';'+self.__apellido+';'+self.__millasacum
+    def returnnumviajero(self):
+        return self.__numviajero
+    def canttotalmillas(self):
+        return self.__millasacum
+    def acumularmillas(self,act):
+        self.__millasacum+=act
+        return self.__millasacum
+    
+    def canjearmillas(self,millascanj):
+        if millascanj<=self.__millasacum:
+            self.__millasacum-=millascanj
+        else: print('ERROR')
+    def __gt__(self,otroViajero): #Sobrecarga de operador mayor
+        if self.__millasacum>otroViajero.__millasacum:
+            return True
+        else: return False
