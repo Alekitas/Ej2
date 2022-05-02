479a99721ef3547a519baa6173439a5b4b062d57
Alejo Atienza <73915312+Alekitas@users.noreply.github.com>
2022-05-02 19:31:06 -0300

    Update ModuloMain.py

--- Ej2/ModuloMain.py
+++ Ej2/ModuloMain.py
@@ -1,32 +1,35 @@
-class ViajeroFrecuente:
-    __numviajero=''
-    __DNI=''
-    __nombre=''
-    __apellido=''
-    __millasacum=''
-    
-    def __init__(self,numviajero,DNI,nombre,apellido,millasacum):
-        self.__numviajero=numviajero
-        self.__DNI=DNI
-        self.__nombre=nombre
-        self.__apellido=apellido
-        self.__millasacum=millasacum
-    
-    def muestra(self):
-        return self.__numviajero+';'+self.__DNI+';'+self.__apellido+';'+self.__millasacum
-    def returnnumviajero(self):
-        return self.__numviajero
-    def canttotalmillas(self):
-        return self.__millasacum
-    def acumularmillas(self,act):
-        self.__millasacum+=act
-        return self.__millasacum
-    
-    def canjearmillas(self,millascanj):
-        if millascanj<=self.__millasacum:
-            self.__millasacum-=millascanj
-        else: print('ERROR')
-    def __gt__(self,otroViajero): #Sobrecarga de operador mayor
-        if self.__millasacum>otroViajero.__millasacum:
-            return True
-        else: return False
+from ClaseViajeroFrecuente import ViajeroFrecuente
+import csv
+
+if __name__=='__main__':
+    lista=[]
+    archivo=open('Libro1.csv')
+    reader=csv.reader(archivo,delimiter=';')
+    for fila in reader:
+        numviajero=fila[0]
+        DNI=int(fila[1])
+        nombre=fila[2]
+        apellido=fila[3]
+        millasacum=int(fila[4])
+        unViajero=ViajeroFrecuente(numviajero,DNI,nombre,apellido,millasacum)
+        lista.append(unViajero)
+    archivo.close()
+    opcion=0
+    numv=int(input('Ingrese numero de viajero: '))
+    while opcion!=-1:
+        print('\n1- Consultar Cantidad de Millas Acumuladas\n')
+        print('2-Acumular Millas\n')
+        print('3-Canjear Millas\n')
+        print('4-Ingrese -1 para finalizar\n')
+        opcion=int(input('Ingrese una opcion: '))
+        if opcion==1:
+            for i in range(len(lista)):
+                print('Cantidad total de millas acumuladas del viajero: ',lista[i].returnnumviajero(),';',lista[i].canttotalmillas())
+        elif opcion==2:
+            act=int(input('Ingrese cantidad de millas del ultimo viaje: '))
+            lista[numv-1].acumularmillas(int(act))
+            print('Cantidad de millas actualizada: ',lista[numv-1].canttotalmillas())
+        elif opcion==3:
+            mcanj=int(input('Ingrese cantidad de millas a canjear: '))
+            lista[numv-1].canjearmillas(mcanj)
+            print('Millas actuales: ',lista[numv-1].canttotalmillas())
